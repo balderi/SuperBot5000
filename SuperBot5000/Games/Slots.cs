5455c6f202a959b78e7d0dcfb9056516a39838a1
Balder <balderi@tsponline.dk>
2020-07-27 01:55:02 +0200

    Tweak slots (again again)

--- SuperBot5000/Games/Slots.cs
+++ SuperBot5000/Games/Slots.cs
@@ -127,10 +127,10 @@ namespace SuperBot5000.Games
             int val2 = StaticResources.GetRandomSlotsValue();
             int val3 = StaticResources.GetRandomSlotsValue();
 
-            if(val1 == 7 && rnd.Next(0, 2) == 0)
+            if(val1 == 7 && rnd.Next(0, 3) > 0)
                 val1 = (val1 + StaticResources.GetRandomSlotsValue()) % StaticResources.GetTotalSlots();
 
-            if ((val1 == val2 || val1 == val3) && rnd.Next(0, 2) == 0)
+            if ((val1 == val2 || val2 == val3) && rnd.Next(0, 2) == 0)
                 val2 = (val2 + StaticResources.GetRandomSlotsValue()) % StaticResources.GetTotalSlots();
 
             if ((val2 == val3 || val1 == val3) && rnd.Next(0, 2) == 0)
