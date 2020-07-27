66a1b8bc98ff1be9fa082fedfe6971f3a139b44c
Balder <balderi@tsponline.dk>
2020-07-27 03:36:20 +0200

    Check users every time timer ticks

--- SuperBot5000/Program.cs
+++ SuperBot5000/Program.cs
@@ -54,9 +54,6 @@ namespace SuperBot5000
                 Environment.GetEnvironmentVariable("DiscordToken"));
             await _client.StartAsync();
 
-            _users = UserList.GetUserList().Users;
-            _names = _users.Select(x => x.Name).ToList();
-
             _timer = new System.Timers.Timer(10000);
             _timer.Elapsed += Timer_Tick;
 
@@ -68,6 +65,8 @@ namespace SuperBot5000
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            _users = UserList.GetUserList().Users;
+            _names = _users.Select(x => x.Name).ToList();
             var olUsers = _client.GetGuild(252801284439015424).Users.Where(x => _names.Contains(x.Mention)).Select(x => x.Mention).ToList();
             foreach(User u in _users.Where(x => olUsers.Contains(x.Name)))
             {
