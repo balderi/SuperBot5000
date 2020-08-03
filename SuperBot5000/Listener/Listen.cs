c6bb6ae70b8ae42d66be8b4e422f8b491d76bcd8
Balder <balderi@tsponline.dk>
2020-08-03 03:50:09 +0200

    That's more like it...

--- SuperBot5000/Listener/Listen.cs
+++ SuperBot5000/Listener/Listen.cs
@@ -8,12 +8,12 @@ namespace SuperBot5000.Listener
     {
         public static bool ForKeyWord(SocketUserMessage message)
         {
-            if(Regex.IsMatch(message.Content, @"(^|\. )[^ ]+ (was|is|should|has|had|took)"))
+            var res = Regex.IsMatch(message.Content, @"(^|\. )[^ ]+ (was|is|should|has|had|took)");
+            if (res)
             {
                 message.Channel.SendMessageAsync($"{message.Author.Mention} {Regex.Replace(message.Content, @"(^|\. )[^ ]+ (was|is|should|has|had|took)", "$1" + "Your mom " + "$2")}");
-                return true;
             }
-            return false;
+            return res;
         }
     }
 }
