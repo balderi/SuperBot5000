bc20dd28f5cb84af9ad990c1b50c6eaa22c80175
Balder <balderi@tsponline.dk>
2020-09-11 19:58:08 +0200

    lol

--- SuperBot5000/Listener/Listen.cs
+++ SuperBot5000/Listener/Listen.cs
@@ -8,6 +8,8 @@ namespace SuperBot5000.Listener
     {
         public static bool ForKeyWord(SocketUserMessage message)
         {
+            var lol = new string[]{ "ROFL", "ROFLMAO", "OLOLOLOLOL!!!!11!!one!!!!eleventyone", "LOL", "BWAHAHA" };
+            var rnd = new Random();
             Console.WriteLine("Listening for keywords");
             var regex = @"(^|\. )[^ ]+('s| was| is| should| has| had| took)";
             var res = Regex.IsMatch(message.Content, regex);
@@ -15,7 +17,18 @@ namespace SuperBot5000.Listener
             {
                 Console.WriteLine("Keywords found");
                 message.Channel.SendMessageAsync($"{message.Author.Mention} {Regex.Replace(message.Content, regex, "$1" + "Your mom" + "$2")}");
+                return res;
             }
+
+            regex = @".*(lol|ha|hah|haha|hahaha|lul|lel|lulz).*";
+            res = Regex.IsMatch(message.Content.ToLower(), regex);
+            if (res)
+            {
+                Console.WriteLine("Keywords found");
+                message.Channel.SendMessageAsync($"{lol[rnd.Next(0, lol.Length)]}");
+                return res;
+            }
+
             return res;
         }
     }
