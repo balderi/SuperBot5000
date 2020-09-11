6407717de08eaf5dc60bef59f8cc429c39ed0a6a
Balder <balderi@tsponline.dk>
2020-09-11 19:49:48 +0200

    test

--- SuperBot5000/Listener/Listen.cs
+++ SuperBot5000/Listener/Listen.cs
@@ -9,12 +9,12 @@ namespace SuperBot5000.Listener
         public static bool ForKeyWord(SocketUserMessage message)
         {
             Console.WriteLine("Listening for keywords");
-            var regex = @"(^|\. )[^ ]+ (was|is|should|has|had|took)";
+            var regex = @"(^|\. )[^ ]+ ('s| was| is| should| has| had| took)";
             var res = Regex.IsMatch(message.Content, regex);
             if (res)
             {
                 Console.WriteLine("Keywords found");
-                message.Channel.SendMessageAsync($"{message.Author.Mention} {Regex.Replace(message.Content, regex, "$1" + "Your mom " + "$2")}");
+                message.Channel.SendMessageAsync($"{message.Author.Mention} {Regex.Replace(message.Content, regex, "$1" + "Your mom" + "$2")}");
             }
             return res;
         }
