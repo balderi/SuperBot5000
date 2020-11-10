1ad0105a54e5d950106cf1d2ef06b3f5a1be39d6
Balder <balderi@tsponline.dk>
2020-11-10 01:43:43 +0100

    Update DI to 5.0.0

--- SuperBot5000/Modules/AudioModule.cs
+++ SuperBot5000/Modules/AudioModule.cs
@@ -12,32 +12,25 @@ namespace SuperBot5000.Modules
     {
         private readonly AudioService _service;
 
-        // Remember to add an instance of the AudioService
-        // to your IServiceCollection when you initialize your bot
         public AudioModule(AudioService service)
         {
             _service = service;
         }
 
-        // You *MUST* mark these commands with 'RunMode.Async'
-        // otherwise the bot will not respond until the Task times out.
         [Command("join", RunMode = RunMode.Async)]
-        public async Task JoinCmd()
+        public async Task JoinAsync()
         {
             await _service.JoinAudio(Context.Guild, (Context.User as IVoiceState).VoiceChannel);
         }
 
-        // Remember to add preconditions to your commands,
-        // this is merely the minimal amount necessary.
-        // Adding more commands of your own is also encouraged.
         [Command("leave", RunMode = RunMode.Async)]
-        public async Task LeaveCmd()
+        public async Task LeaveAsync()
         {
             await _service.LeaveAudio(Context.Guild);
         }
 
         [Command("play", RunMode = RunMode.Async)]
-        public async Task PlayCmd([Remainder] string song)
+        public async Task PlayAsync([Remainder] string song)
         {
             await _service.SendAudioAsync(Context.Guild, Context.Channel, song);
         }
